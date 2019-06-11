ca56a84d4749a747fc2694a82b883a6199bd1023
Sooraj99-ui <51689620+Sooraj99-ui@users.noreply.github.com>
2019-06-11 10:24:48 +0530

    Update codekata3.c

--- codekata3.c
+++ codekata3.c
@@ -18,7 +18,7 @@ int main()
         case 'E':
         case 'I':
         case 'O':
-        case 'U':printf("Vowels");
+        case 'U':printf("Vowel");
                  break;
         default:printf("Consonant");
         }
