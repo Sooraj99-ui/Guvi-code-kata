4bf26b50c16431eae351802a2db2f64dcc3414c2
Sooraj99-ui <51689620+Sooraj99-ui@users.noreply.github.com>
2019-06-11 12:11:46 +0530

    update codekatahunter3.c

--- codekatahunter3.c
+++ codekatahunter3.c
@@ -5,12 +5,16 @@ int main()
     int n;
     scanf("%d", &n);
     int k;
+    int found = 0;
 
     for(int i = 0; i < n; i++) {
         scanf("%d", &k);
-        if(k == i)
+        if(k == i) {
             printf("%d ", k);
+            found = 1;
+        }
     }
 
+    if(!found) printf("-1");
     return 0;
 }
