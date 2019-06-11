18ff7ae643d410ef8d835c128276baaca73564ed
Sooraj99-ui <51689620+Sooraj99-ui@users.noreply.github.com>
2019-06-11 14:33:18 +0530

    update codekataplayer2.c

--- codekataplayer2.c
+++ codekataplayer2.c
@@ -1,13 +1,27 @@
-//sag;ib;sa;ih;ihb
-#include<stdio.h>
-int main()
-{
-    int n,fact=1,i;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    #include <stdio.h>
+     
+    int main()
     {
-        fact*=i;
+       char s[1000], r[1000];
+       int begin, end, count = 0;
+     
+      scanf("%s",s);
+     
+       // Calculating string length
+     
+       while (s[count] != '\0')
+          count++;
+     
+       end = count - 1;
+     
+       for (begin = 0; begin < count; begin++) {
+          r[begin] = s[end];
+          end--;
+       }
+     
+       r[begin] = '\0';
+     
+       printf("%s\n", r);
+     
+       return 0;
     }
-    printf("%d",fact);
-    return 0;
-}
