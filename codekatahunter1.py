45152242b0ba27d191d1f18777bb95bb6f1d1049
Sooraj99-ui <51689620+Sooraj99-ui@users.noreply.github.com>
2019-06-11 11:59:06 +0530

    Add files via upload

---
+++ codekatahunter1.py
@@ -0,0 +1,21 @@
+# somfonrgiun
+
+from collections import *
+
+n = int(input())
+l = [int(i) for i in input().split(' ')]
+d = defaultdict(int)
+rep = []
+
+for i in range(n):
+    d[l[i]] += 1
+    if d[l[i]] == 2:
+        rep.append(l[i])
+
+rep.sort()
+if len(rep) == 0:
+    print("unique")
+else:
+    for i in range(len(rep)):
+        print(rep[i], end=" ")
+
